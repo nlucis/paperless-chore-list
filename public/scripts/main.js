70f0959770c1badd039ab2f3f9d8ec300a5724c9
Arjay Jones <arjay.jones@nlucis.dev>
2023-01-07 12:03:42 -0800

    added some premade avatar images courtesy of stable diffusion

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,14 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('i hate plain javascript');
+
+  /*  
+   *  Instead of using a centralized DB, because I am poor AF and cant afford that,
+   *  Use per-device local storage of avatar and name values, and a socket.io implementation
+   *  to share those values among other users when connected.
+  */
+
+  // Just for scaffolding so I dont have to hardcode that shizz
+  const avatarImage = document.createElement();
+  avatarImage.setAttribute('class', 'avatar');
 });
