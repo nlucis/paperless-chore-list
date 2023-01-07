5d299a6839d988d3575f56998fde28f1318f3f69
Arjay Jones <arjay.jones@nlucis.dev>
2023-01-07 12:22:37 -0800

    stubbed out avatar refs via local storage

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('i hate plain javascript');
 
   /*  
    *  Instead of using a centralized DB, because I am poor AF and cant afford that,
    *  Use per-device local storage of avatar and name values, and a socket.io implementation
    *  to share those values among other users when connected.
   */
+  const persistence = window.localStorage;
+  persistence.setItem('avatar', 'avatar1');
+  persistence.setItem('name', 'Awwjay');
 
-  // Just for scaffolding so I dont have to hardcode that shizz
-  const avatarCount = 12;
-  const indexStart = 1; // not zero-indexed in other words
-  
-  for (let aNum = indexStart; aNum <= avatarCount; aNum++) {
-    const avatarImage = document.createElement('img');
-    avatarImage.src = `public/assets/avatar${aNum}.jpeg`;
-    avatarImage.setAttribute('class', 'avatar');
-    document.getElementById('avatar-list').appendChild(avatarImage);
-  }
+  const avatarURI = `public/assets/${persistence.getItem('avatar')}.jpeg`;
+
+  const userAvatar = document.createElement('img');
+  userAvatar.src = avatarURI;
+  userAvatar.setAttribute('class', 'avatar');
+
+  document.getElementById('avatar-list').appendChild(userAvatar);
 });
