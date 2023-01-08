168ab5034eefc3596391ad11898630391127f85c
Arjay Jones <arjay.jones@nlucis.dev>
2023-01-07 18:26:20 -0800

    removed import statement in global.css

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -17,15 +17,11 @@ document.addEventListener('DOMContentLoaded', () => {
   persistence.setItem('avatar', 'avatar1');
   persistence.setItem('name', 'Awwjay');
 
-  console.log(userPrefs);
-  userPrefs.name = "Unga Bunga Man"
-  console.log(userPrefs);
-
   const avatarURI = `public/assets/${persistence.getItem('avatar')}.jpeg`;
-
   const userAvatar = document.createElement('img');
   userAvatar.src = avatarURI;
   userAvatar.setAttribute('class', 'avatar');
 
   document.getElementById('avatar-list').appendChild(userAvatar);
+  
 });
