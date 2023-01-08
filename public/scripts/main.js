46d727c0b01cb68262f659058ccba5710246db6d
Arjay Jones <arjay.jones@nlucis.dev>
2023-01-07 19:02:16 -0800

    removed buttons to add new categories and sections since these values are set by the tasks data file

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,27 +1,28 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-
   /*  
    *  Instead of using a centralized DB, because I am poor AF and cant afford that,
-   *  Use per-device local storage of avatar and name values, and a socket.io implementation
+   *  Use per-device local storage of avatar and name values, and some form of lite implementation
    *  to share those values among other users when connected.
   */
   const persistence = window.localStorage;
-
   const userPrefs = {
     name: 'Awwjay',
     avatarID: 'avatar1'
   };
-
   persistence.setItem('userPrefs', userPrefs);
-  persistence.setItem('avatar', 'avatar1');
-  persistence.setItem('name', 'Awwjay');
 
+  // * -- User Avatar --
   const avatarURI = `public/assets/${persistence.getItem('avatar')}.jpeg`;
   const userAvatar = document.createElement('img');
   userAvatar.src = avatarURI;
   userAvatar.setAttribute('class', 'avatar');
 
-  document.getElementById('avatar-list').appendChild(userAvatar);
-  
+  // * -- User Name --
+  const usernameLabel = document.createElement('h5');
+
+  document.getElementById('name-badge').append(
+    userAvatar, 
+    usernameLabel
+  );
 });
